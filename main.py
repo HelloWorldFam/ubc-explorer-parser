f226fc46ea5e1b478940b648efcb1df290653269
Jerry Hall <hall.jerry9@gmail.com>
2021-07-07 21:40:42 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# call ubcalend_text.py and ubcexplorerscript.py
+## Assume everything is done
+# assume you have final_course which is an array
+
+from ubcexplorerscript import UBCExplorerScript
+
+ubcexplorerscriptobj = UBCExplorerScript()
