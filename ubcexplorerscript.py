f7e2e35b9606ee58c9d6bb723efbe079425b9d7d
scott-m-king <scott.m.king@outlook.com>
2021-07-10 01:41:27 -0700

    helper functions

--- ubcexplorerscript.py
+++ ubcexplorerscript.py
@@ -2,15 +2,42 @@ import re
 from ubcalend_txt import Scraper
 
 class UBCExplorerScript:
+    def __credits_to_int__(self, course):
+        if "cred" in course and len(course["cred"]) == 1:
+            course["cred"] = int(course["cred"])
+        else:
+            course["cred"] = None
+        return course
+
+
+    def __parse_prereqs__(self, course):
+        if "preq" in course:
+            course["preq"] = re.findall(r'[A-Z]*\s\d{3}[A-Z]*', course["preq"])
+        if "creq" in course:
+            course["creq"] = re.findall(r'[A-Z]*\s\d{3}[A-Z]*', course["creq"])
+        return course
+
+
+    def __get_dependencies__(self, unprocessed):
+        courses_array = unprocessed.copy()
+        for course in courses_array:
+            for course2 in courses_array:
+                if "preq" in course2:
+                    if "code" in course and "preq" in course2 and course["code"] in course2["preq"]:
+                        if not "depn" in course:
+                            course["depn"] = []
+                        course["depn"].append(course2["code"])
+        return courses_array
+
     def __get_course_links__(self, course):
         code = course["code"].split(" ")
         course["link"] = "https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-course&dept=" + code[0] + "&course=" + code[1]
         return course
 
+
     def __generate_json_array__(self, calendar_data):
         courses_array = []
         course = {}
-        codes = []
 
         for line in calendar_data:
             if len(line) > 3 and line != 'newline':
@@ -23,29 +50,9 @@ class UBCExplorerScript:
                 course.clear()
                 continue
 
-        for course in courses_array:
-            if "cred" in course and len(course["cred"]) == 1:
-                course["cred"] = int(course["cred"])
-            else:
-                course["cred"] = None
-            
-            if "dept" in course:
-                codes.append(course["dept"])
-
-        for course in courses_array:
-            if "preq" in course:
-                course["preq"] = re.findall(r'[A-Z]*\s\d{3}[A-Z]*', course["preq"])
-            if "creq" in course:
-                course["creq"] = re.findall(r'[A-Z]*\s\d{3}[A-Z]*', course["creq"])
-
-        for course in courses_array:
-            for course2 in courses_array:
-                if "preq" in course2:
-                    if "code" in course and "preq" in course2 and course["code"] in course2["preq"]:
-                        if not "depn" in course:
-                            course["depn"] = []
-                        course["depn"].append(course2["code"])
-
+        courses_array = [self.__credits_to_int__(course) for course in courses_array]
+        courses_array = [self.__parse_prereqs__(course) for course in courses_array]
+        courses_array = self.__get_dependencies__(courses_array)
         return [self.__get_course_links__(course) for course in courses_array]
         
 
