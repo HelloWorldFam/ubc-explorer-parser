86c6746848a91e96b33c869eb83be94110122ff9
Scott <scott.m.king@outlook.com>
2022-02-24 15:38:50 -0800

    Comment out testing stuff

--- ubcexplorerscript.py
+++ ubcexplorerscript.py
@@ -43,7 +43,7 @@ class UBCExplorerScript:
         return self.__generate_json_array(calendar_data)
 
 # testing
-script = UBCExplorerScript()
-output = script.main()
-with open('output.json', 'w') as file:
-    json.dump(output, file)
+# script = UBCExplorerScript()
+# output = script.main()
+# with open('output.json', 'w') as file:
+#     json.dump(output, file)
