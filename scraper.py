69f2a21cc6dda34fba684c00f2170cfe6635e558
Scott <scott.m.king@outlook.com>
2022-02-25 20:02:58 -0800

    fix issue with 3 letter courses

--- scraper.py
+++ scraper.py
@@ -24,21 +24,19 @@ class Scraper:
             if details.name == "dt":
                 new_course["dept"] = split[0]
                 new_course["code"] = f"{split[0]} {split[1]}"
-                new_course["cred"] = split[2][1:-1]  # (3) -> 3, (1-6) -> 1-6
+                new_course["cred"] = self.__credits_to_int(split[2][1:-1])  # (3) -> 3, (1-6) -> 1-6
                 new_course["link"] = f"{self.courses_url}&dept={split[0]}&course={split[1]}"
                 new_course["name"] = details.find("b").text
             elif details.name == "dd":
-                new_course["desc"] = text.split("\n")[0].strip()
+                new_course["desc"] = self.__strip(text.split("\n")[0])
 
                 if "Prerequisite:" in text:
-                    prereqs = text.split("Prerequisite:")[1].split("Corequisite:")[0].split("Equivalency:")[0]
-                    new_course["prer"] = prereqs.strip()
-                    new_course["preq"] = self.__parse_reqs(prereqs)
+                    preq_text = self.__strip(text.split("Prerequisite:")[1].split("Corequisite:")[0].split("Equivalency:")[0])
+                    self.__process_reqs(preq_text, new_course, "prer", "preq")
 
                 if "Corequisite:" in text:
-                    coreqs = text.split("Corequisite:")[1].split("Equivalency:")[0]
-                    new_course["crer"] = coreqs.strip()
-                    new_course["creq"] = self.__parse_reqs(coreqs)
+                    creq_text = self.__strip(text.split("Corequisite:")[1].split("Equivalency:")[0])
+                    self.__process_reqs(creq_text, new_course, "crer", "creq")
 
                 # skip grad
                 if int(new_course["code"].split(" ")[1]) < 500:
@@ -47,8 +45,18 @@ class Scraper:
 
         return result
 
-    def __parse_reqs(self, blurb) -> List[str]:
-        return re.findall(r"\b[A-Z]{3,4} \d{3}\b", blurb.strip())
+    def __process_reqs(self, text: str, course: dict, text_label: str, req_label: str) -> List[str]:
+        course[text_label] = text
+        reqs = re.findall(r"\b[A-Z]{3,4} \d{3}\b", text)
+        if len(reqs):
+            course[req_label] = reqs
+
+    def __strip(self, text: str):
+        return re.sub(r"[ ]{2,}", " ", text).strip()
+
+    def __credits_to_int(self, creds: str):
+        return int(creds) if creds.isnumeric() else None
+        
 
     def main(self) -> List[dict]:
         html = urllib.request.urlopen(self.base_url)
--- ubcexplorerscript.py
+++ ubcexplorerscript.py
@@ -3,39 +3,33 @@ from scraper import Scraper
 import json
 
 class UBCExplorerScript:
-    # for a given course, converts the credits value from string to integer
-    # returns the modified course
-    def __credits_to_int(self, course: dict) -> dict:
-        if "cred" in course and len(course["cred"]) >= 1 and course["cred"].isnumeric():
-            course["cred"] = int(course["cred"])
-        else:
-            course["cred"] = None
-        return course
-
     # for each course in the course array, search for all instances where the current course is a prereq for another course
     # then adds courses found to the 'depn' array which represents the list of dependencies for that course
     # returns the modified course array
-    def __get_dependencies(self, unprocessed: List[dict]) -> List[dict]:
-        courses_array = unprocessed.copy()
-        for course in courses_array:
-            for course2 in courses_array:
-                if "preq" in course2:
-                    if "code" in course and "preq" in course2 and course["code"] in course2["preq"]:
-                        if not "depn" in course:
-                            course["depn"] = []
-                        course["depn"].append(course2["code"])
-        for course in courses_array:
+    def __get_dependencies(self, courses_array: List[dict]) -> List[dict]:
+        courses_map = {course["code"]:course for course in courses_array}
+        courses_with_preqs = [course for course in courses_array if "preq" in course]
+
+        for course in courses_with_preqs:
+            for preq in course["preq"]:
+                if preq in courses_map and preq != course["code"]:
+                    to_update = courses_map[preq]
+                    if not "depn" in to_update:
+                        to_update["depn"] = set()
+                    to_update["depn"].add(course["code"])
+                
+
+        for course in courses_map.values():
             if "depn" in course:
-                course["depn"] = sorted(course["depn"])
-        return courses_array
+                course["depn"] = sorted(list(course["depn"]))
+                
+        return [value for value in courses_map.values()]
 
     # generates final list of course objects to be uploaded to mongo
     # returns final course array
-
     def __generate_json_array(self, calendar_data: List[dict]) -> List[dict]:
-        courses_array = calendar_data
-        courses_array = sorted([self.__credits_to_int(course) for course in courses_array], key=lambda course: course['code'])
-        return self.__get_dependencies(courses_array)
+        sorted_courses = sorted(calendar_data, key=lambda course: course['code'])
+        return self.__get_dependencies(sorted_courses)
 
     def main(self) -> List[dict]:
         scraper = Scraper()
@@ -43,7 +37,7 @@ class UBCExplorerScript:
         return self.__generate_json_array(calendar_data)
 
 # testing
-# script = UBCExplorerScript()
-# output = script.main()
-# with open('output.json', 'w') as file:
-#     json.dump(output, file)
+script = UBCExplorerScript()
+output = script.main()
+with open('output.json', 'w') as file:
+    json.dump(output, file)
